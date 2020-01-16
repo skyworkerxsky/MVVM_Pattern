deff30e9a513432db444cb53462bb6c0955ac7aa
skyworkerxsky <pwuser@yandex.ru>
2020-01-16 15:48:56 +0500

    tableView with mvc

---
+++ MVVM-2/MVVM-2/Model/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  MVVM-2
+//
+//  Created by Алексей Макаров on 16.01.2020.
+//  Copyright © 2020 Алексей Макаров. All rights reserved.
+//
+
+import Foundation
+
+struct Profile {
+    
+    var name: String
+    var secondName: String
+    var age: Int
+}
---
+++ MVVM-2/MVVM-2/View/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  MVVM-2
+//
+//  Created by Алексей Макаров on 16.01.2020.
+//  Copyright © 2020 Алексей Макаров. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+}
